7325dd0e47dde9a588f47e5a9bc338bb82e25e20
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-10-10 02:46:59 +0300

    IT PARSES!

--- Activity.hs
+++ Activity.hs
@@ -1,5 +1,21 @@
 {-# LANGUAGE TemplateHaskell #-}
 
+{--
+#!/bin/bash  
+wnd_focus=$(xdotool getwindowfocus)
+wnd_title=$(xprop -id $wnd_focus WM_NAME)
+wnd_class=$(xprop -id $wnd_focus WM_CLASS)
+echo -ne "${wnd_class}\n${wnd_title}"
+--}
+
+{--
+# Xwm output example:
+WM_CLASS(STRING) = "Navigator", "Firefox"
+WM_NAME(STRING) = "acladd screen site:www.gnu.org at DuckDuckGo - Vimperator (Private Browsing)"
+# To be parsed to
+Activity { _classes = ["Navigator", "Firefox"], _name="acladd..sing)", _actions = ???, _time = !!! }
+--}
+
 module Activity
   ( Activity(..)
    ,getCurrentActivity ) where
@@ -11,20 +27,32 @@ import System.Process
 import Control.Lens
 
 data Activity = Activity {
-                 _classes :: String 
+                 _classes :: [String]
                 ,_name    :: String 
                 ,_actions :: Integer
                 ,_time    :: Integer } deriving (Show, Read)
 
 makeLenses ''Activity
 
-a :: Activity
-a = Activity "" "" 0 (0, 0)
+mk_a :: Activity
+mk_a = Activity{ _classes = [], _name = "", _actions = 0, _time = 0 }
 
-p :: [String] -> Activity
-p _ = a
+parse :: [String] -> Activity
+parse (s:ss) = parse_do (s:ss) mk_a
+
+parse_do :: [String] -> Activity -> Activity
+parse_do [] a = a
+parse_do ("WM_CLASS(STRING)":"=":ss) a = parse_classes ss a
+
+parse_classes :: [String] -> Activity -> Activity
+parse_classes ("WM_NAME(STRING)":"=":ss) a = parse_name ss a
+parse_classes (x:ss) a = parse_classes ss (a & classes %~ (\xs -> x:xs))
+
+parse_name :: [String] -> Activity -> Activity
+parse_name [] a = a
+parse_name (x:ss) a = parse_name ss (a & name <>~ (" " ++ x))
 
 getCurrentActivity :: IO Activity
 getCurrentActivity = createProcess (proc "xwm" []){ std_out = CreatePipe } >>=
                      \(_, Just h1, _, _) -> hGetContents h1                >>=
-                     \s                  -> return $ p $ words s
+                     \s                  -> return (parse (words s))
